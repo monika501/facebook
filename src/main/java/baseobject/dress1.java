ae482d858dcec841d73357ad0e7914776c3a9eb7
unknown <srmonika501@gmail.com>
2020-01-08 09:57:35 +0530

    basebage modified

--- src/main/java/baseobject/dress1.java
+++ src/main/java/baseobject/dress1.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class dress1 extends basepage {
+public class dress1 extends basepage123 {
 	
 
 	@FindBy(xpath="(//a[@title='Dresses'])[2]")
